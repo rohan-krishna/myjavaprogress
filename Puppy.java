d62cf01beaf8a15a4aec4b7aaff92a091ac5238f
Rohan Krishna <rohan@bluehexagon.in>
2018-12-21 21:32:56 +0530

    Learning methods and instance variables.

---
+++ Puppy.java
@@ -0,0 +1,9 @@
+public class Puppy {
+    public Puppy(String name) {
+        System.out.println("The Puppy's name is: " + name);
+    }
+
+    public static void main(String args[]) {
+        Puppy mypuppy = new Puppy("Chandler!");
+    }
+}
